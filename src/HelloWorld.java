604afe35198489a8c0f00abe9837aa363d3b20ec
izthekid <isaiah.santos@ocsbstudent.ca>
2017-09-18 13:15:04 -0400

    bye iz the kid

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Iz the KIDDDD");
+		System.out.println("Bye Iz the KIDDDD");
 		System.out.println("Isn't this fun???");
 		System.out.println("IzTheKid said \"wagwan\"");
 		System.out.println("Diegoo said \" im posted\"");
