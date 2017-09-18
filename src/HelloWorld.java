c54d056ffcad068f29a3b3b049645eb5c90f854d
izthekid <isaiah.santos@ocsbstudent.ca>
2017-09-18 13:06:58 -0400

    first save

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Iz the KIDDDD");
+		System.out.println("Isn't this fun???");
+		System.out.println("IzTheKid said \"wagwan\"");
+		System.out.println("Diegoo said \" im posted\"");
+	}
+
+}
